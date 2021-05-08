8cf53a821998c57430b9859235a2acae8505e6a6
冯文议 <xfsy_2015@163.com>
2021-05-08 10:57:41 +0800

    init

---
+++ demo-springboot/src/main/java/com/fengwenyi/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-05-08
+ */
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+}
---
+++ demo-springboot/src/test/java/com/fengwenyi/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-05-08
+ */
+@SpringBootTest
+public class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
