994894e0abda51cc9137b537f946190e841e9e3b
冯文议 <xfsy_2015@163.com>
2021-05-21 09:53:10 +0800

    feat: spring boot csv demo

---
+++ demo-springboot-csv/src/main/java/com/fengwenyi/demospringbootcsv/DemoSpringBootCsvApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringbootcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-05-21
+ */
+@SpringBootApplication
+public class DemoSpringBootCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootCsvApplication.class, args);
+    }
+
+}
