23e748e1b3db0cc79e9061b3daadb8439fd08e61
冯文议 <xfsy_2015@163.com>
2021-08-18 17:58:41 +0800

    spring boot webflux init

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [ ] [demo-spring-boot-aop](./demo-spring-boot-aop): Spring Boot 集成 AOP 示例
 - [x] [demo-spring-boot-event](demo-spring-boot-event): spring boot event 示例
 - [ ] [demo-spring-boot-hello](./demo-spring-boot-hello): spring boot hello
+- [x] [demo-spring-boot-webflux](demo-spring-boot-webflux): Spring Boot WebFlux 示例
 - [x] [demo-spring-boot-websocket](./demo-spring-boot-websocket): Spring Boot 集成 WebSocket 示例
   
 
---
+++ demo-spring-boot-webflux/src/main/java/com/fengwenyi/demospringbootwebflux/DemoSpringBootWebFluxApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringbootwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-08-18
+ */
+@SpringBootApplication
+public class DemoSpringBootWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootWebFluxApplication.class, args);
+    }
+
+}
