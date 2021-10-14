98eeb7ca4e3743ad18f5e63b68237dfaa3743d3a
冯文议 <xfsy_2015@163.com>
2021-10-14 17:57:27 +0800

    test Scheduled fixRate failed....

--- demo-spring-boot-scheduled/src/main/java/com/fengwenyi/demospringbootscheduled/config/ScheduledConfiguration.java
+++ demo-spring-boot-scheduled/src/main/java/com/fengwenyi/demospringbootscheduled/config/ScheduledConfiguration.java
@@ -1,6 +1,7 @@
 package com.fengwenyi.demospringbootscheduled.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -9,5 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @Configuration
 @EnableScheduling
+@EnableAsync
 public class ScheduledConfiguration {
 }
--- demo-spring-boot-scheduled/src/main/java/com/fengwenyi/demospringbootscheduled/task/ScheduledTask.java
+++ demo-spring-boot-scheduled/src/main/java/com/fengwenyi/demospringbootscheduled/task/ScheduledTask.java
@@ -1,9 +1,14 @@
 package com.fengwenyi.demospringbootscheduled.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,8 +27,42 @@ public class ScheduledTask {
 
     // 间隔5秒执行一次
     //@Scheduled(fixedDelay = 5000)
-    @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS)
+    //@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS)
     public void testFixedDelay() {
         log.info("test fixedDelay");
+    }
+
+    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    public void testFixedRate() {
+        test();
+    }
+
+    @Async
+    public void test() {
+        String traceId = UUID.randomUUID().toString().replaceAll("-", "");
+        log.info("test fixedRate {} start ", traceId);
+        int sleepTime = ThreadLocalRandom.current().nextInt(10);
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("test fixedRate {} end {}", traceId, sleepTime);
+    }
+
+    private static final ExecutorService exec = Executors.newFixedThreadPool(10);
+
+
+    public static class SubThread extends Thread {
+        private final int i;
+
+        public SubThread(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
