012dd24a37bbd291054e22d9692be0ad0f088c6a
冯文议 <xfsy_2015@163.com>
2021-12-20 14:31:04 +0800

    java log 体系

---
+++ demo-spring-boot-log/src/main/java/com/fengwenyi/demospringbootlog/DemoSpringBootLogApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringbootlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2021-12-20
+ */
+@SpringBootApplication
+public class DemoSpringBootLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootLogApplication.class, args);
+    }
+
+}
