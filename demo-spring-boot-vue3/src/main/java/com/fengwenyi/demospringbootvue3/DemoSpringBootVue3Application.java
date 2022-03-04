cee5c22235ac9fb3fe61d860effbbf578d8da531
Erwin <xfsy_2015@163.com>
2022-03-05 00:12:39 +0800

    尝试在spring boot中直接引用JS编写VUE3

---
+++ demo-spring-boot-vue3/src/main/java/com/fengwenyi/demospringbootvue3/DemoSpringBootVue3Application.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demospringbootvue3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2022-03-04
+ */
+@SpringBootApplication
+public class DemoSpringBootVue3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootVue3Application.class, args);
+    }
+
+}
---
+++ demo-spring-boot-vue3/src/main/resources/static/js/index.js
@@ -0,0 +1,15 @@
+// const app = Vue.createApp({
+//     data() {
+//         return {
+//             product: 'Socks'
+//         }
+//     }
+// })
+
+const HelloVueApp = {
+    data() {
+        return {
+            message: 'Hello Vue!!'
+        }
+    }
+}
