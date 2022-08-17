69027b3318934e281ba39c3a77cebe37a2a559bc
ErwinFeng <xfsy_2015@163.com>
2022-08-18 00:36:58 +0800

    `@RedisHash`

--- demo-spring-boot-redis/src/main/java/com/fengwenyi/demospringbootredis/model/UserModel.java
+++ demo-spring-boot-redis/src/main/java/com/fengwenyi/demospringbootredis/model/UserModel.java
@@ -2,11 +2,13 @@ package com.fengwenyi.demospringbootredis.model;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.springframework.data.redis.core.RedisHash;
 
 /**
  * @author <a href="https://fengwenyi.com">Erwin Feng</a>
  * @since 2022-08-15
  */
+//@RedisHash
 @Data
 @Accessors(chain = true)
 public class UserModel {
