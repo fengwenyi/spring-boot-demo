e70e353443de8f5781e79de3377b39940b36f056
ErwinFeng <xfsy_2015@163.com>
2022-10-07 01:00:55 +0800

    redisson note demo

--- demo-spring-boot-redisson/src/main/java/com/fengwenyi/springboot/redisson/RedissonController.java
+++ demo-spring-boot-redisson/src/main/java/com/fengwenyi/springboot/redisson/RedissonController.java
@@ -55,46 +55,46 @@ public class RedissonController {
         }
     }
 
-@RequestMapping("/tryLock")
-public String tryLock() {
-    String traceId = IdUtils.genId();
-    log.info("{}, request enter", traceId);
-    RLock rLock = redissonClient.getLock("demo-spring-boot-redisson:tryLock");
-    if (Objects.isNull(rLock)) {
-        log.error("{}, lock null", traceId);
-        return "exception";
-    }
+    @RequestMapping("/tryLock")
+    public String tryLock() {
+        String traceId = IdUtils.genId();
+        log.info("{}, request enter", traceId);
+        RLock rLock = redissonClient.getLock("demo-spring-boot-redisson:tryLock");
+        if (Objects.isNull(rLock)) {
+            log.error("{}, lock null", traceId);
+            return "exception";
+        }
 
-    log.info("{}, get lock", traceId);
+        log.info("{}, get lock", traceId);
 
-    boolean tryLock;
-    try {
-//            tryLock = rLock.tryLock(30, TimeUnit.SECONDS);
-        tryLock = rLock.tryLock(3, 30, TimeUnit.SECONDS);
-    } catch (InterruptedException e) {
-        log.error("{}, lock exception", traceId);
-        return "lock exception";
-    }
+        boolean tryLock;
+        try {
+    //            tryLock = rLock.tryLock(30, TimeUnit.SECONDS);
+            tryLock = rLock.tryLock(3, 30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("{}, lock exception", traceId);
+            return "lock exception";
+        }
 
-    if (!tryLock) {
-        log.error("{}, try lock failed", traceId);
-        return "exception";
-    }
+        if (!tryLock) {
+            log.error("{}, try lock failed", traceId);
+            return "exception";
+        }
 
-    try {
-        TimeUnit.SECONDS.sleep(20);
-        log.info("{}, business success", traceId);
-        return "success";
-    } catch (Exception e) {
-        log.error("{}, business error", traceId);
-        return "failed";
-    } finally {
-        if (rLock.isLocked()) {
-            if (rLock.isHeldByCurrentThread()) {
-                rLock.unlock();
+        try {
+            TimeUnit.SECONDS.sleep(20);
+            log.info("{}, business success", traceId);
+            return "success";
+        } catch (Exception e) {
+            log.error("{}, business error", traceId);
+            return "failed";
+        } finally {
+            if (rLock.isLocked()) {
+                if (rLock.isHeldByCurrentThread()) {
+                    rLock.unlock();
+                }
             }
         }
     }
-}
 
 }
