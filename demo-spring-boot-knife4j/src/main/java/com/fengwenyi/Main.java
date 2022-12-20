41df86a4017e90b03b7f36eed5376d0339414ac7
ErwinFeng <xfsy_2015@163.com>
2022-12-20 16:56:17 +0800

    knife4j demo

---
+++ demo-spring-boot-knife4j/src/main/java/com/fengwenyi/Main.java
@@ -0,0 +1,11 @@
+package com.fengwenyi;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since ${YEAR}-${MONTH}-${DAY}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
