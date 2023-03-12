47186dae4243ecb61851e37e0007bdd85b41a3a2
ErwinFeng <xfsy_2015@163.com>
2023-03-12 16:02:53 +0800

    spring boot http user api demo

---
+++ demo-spring-boot-http/src/main/java/com/fengwenyi/demo/springboot/http/HttpApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demo.springboot.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2023-03-12
+ */
+@SpringBootApplication
+public class HttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpApplication.class, args);
+    }
+
+}
---
+++ demo-spring-boot-http/src/main/java/com/fengwenyi/demo/springboot/http/vo/response/UserResponseVo.java
@@ -0,0 +1,8 @@
+package com.fengwenyi.demo.springboot.http.vo.response;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2023-03-12
+ */
+public class UserResponseVo {
+}
