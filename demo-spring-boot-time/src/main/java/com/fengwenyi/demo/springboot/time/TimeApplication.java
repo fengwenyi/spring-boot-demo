a2789c88d29d2cfd82230aa45aeb260459a74979
ErwinFeng <xfsy_2015@163.com>
2023-08-29 18:14:54 +0800

    feat: 新增 time 模块，主要测试java.time，如 LocalDateTime ...

---
+++ demo-spring-boot-time/src/main/java/com/fengwenyi/demo/springboot/time/TimeApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demo.springboot.time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2023-08-29
+ */
+@SpringBootApplication
+public class TimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeApplication.class, args);
+    }
+
+}
